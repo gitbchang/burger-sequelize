a9451b863a10e82c916ec74a13d9827ea02cebd5
gitbchang <brian.h.chang1@gmail.com>
2017-02-21 15:18:47 -0600

    most functionality is working. need to clean up css

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res){
       .then(function(result){
         var burgerObject = {
           burgs: result          
-        }
+        };
 
         res.render("index", burgerObject);
       });
@@ -22,29 +22,42 @@ router.post("/", function(req, res){
     var newBurger = {
         burger_name: req.body.addBurger,
         
-    }
+    };
     db.burgers.create(newBurger).then(function(result){
         // if you res.json the result, it will redirect and show the newBurger's json object
         // res.json(result);
         res.redirect("/");
         // console.log(result);
-    })
+    });
 });
 
+// Listen for devour update
 router.post("/api/:id", function(req, res){
     db.burgers.update({
         devoured: 1
     },
     {
+
         where: {
             id: req.params.id
         }
     }).then(function(dbPost){
-        console.log(dbPost);
+        // console.log(dbPost);
         res.redirect("/");
     });
 
 });
+
+// Listen for burger owner
+router.post("/api/owner/:id", function(req, res){
+    var newBurgOwner = {
+        ownerName: req.body.name,
+        burgerId: req.params.id
+    }
+    db.burger_owner.create(newBurgOwner).then(function(result){
+        res.json("Owner has been updated");
+    })
+})
 
 
 module.exports = router;
--- public/js/burgDevour.js
+++ public/js/burgDevour.js
@@ -1,8 +1,22 @@
 $(document).on("click", ".devourButton", function(e){
     console.log(e);
     var burgID = $(this).data("id");
+    // var ownerVal = $(".ownerName").val().trim();
+    var ownerVal = getCorrectOwnerValue(burgID);
     
     $.post("/api/"+burgID).then(function(result){
         console.log(result);
     });
+    var ownerObject = {
+        name: ownerVal
+    };
+
+    $.post("/api/owner/"+burgID, ownerObject).then(function(result){
+        console.log(result);
+    });
 });
+
+function getCorrectOwnerValue(burgID){
+    return $(".ownerName[data-ownerid='"+ burgID + "']").val().trim();
+};
+
